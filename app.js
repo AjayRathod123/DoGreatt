9abc7c848577a7d33ee43107d062d8aee6542db2
AjayRathod123 <gamingwithaj2001@gmail.com>
2021-09-22 12:42:11 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -36,20 +36,20 @@ app.get("/work", (req, res) => {
   res.send("Hello in work page");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("/client/build"));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
+}
+
+// __dirname = path.resolve();
 // if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("/client/build"));
+//   app.use(express.static(path.join(__dirname, "client/build")));
 //   app.get("*", (req, res) => {
 //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 //   });
 // }
-
-__dirname = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 app.listen(PORT, (req, res) => {
   console.log(`Listennig at port number ${PORT}`);
