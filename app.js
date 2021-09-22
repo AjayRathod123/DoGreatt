49a848cf55b941589df50c50ae50968498d02e66
AjayRathod123 <gamingwithaj2001@gmail.com>
2021-09-22 12:23:09 +0530

    changes in 2 files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Importing Our Modules
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path");
 
@@ -20,10 +20,6 @@ const PORT = process.env.PORT || 8000;
 
 // Routing
 
-app.get("/", (req, res) => {
-  res.send("Hello in home page");
-});
-
 app.get("/about", (req, res) => {
   res.send("Hello in about page");
 });
@@ -36,20 +32,20 @@ app.get("/work", (req, res) => {
   res.send("Hello in work page");
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-// __dirname = path.resolve();
 // if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
+//   app.use(express.static("/client/build"));
 //   app.get("*", (req, res) => {
 //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 //   });
 // }
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, (req, res) => {
   console.log(`Listennig at port number ${PORT}`);
