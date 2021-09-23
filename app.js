75817212fd6363036663242a7c9bd1e527c17f4c
AjayRathod123 <gamingwithaj2001@gmail.com>
2021-09-23 10:44:32 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ app.get("/work", (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("/client/build"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 // __dirname = path.resolve();
